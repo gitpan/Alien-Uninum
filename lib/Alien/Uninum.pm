b36049f40381d15cec829c7a79f8bf0c011a37bb
Zakariyya Mughal <zmughal@cpan.org>
2013-12-23 22:46:15 +0000

    Import of ZMUGHAL/Alien-Uninum-0.001 from CPAN.
    gitpan-cpan-distribution: Alien-Uninum
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         ZMUGHAL/Alien-Uninum-0.001.tar.gz
    gitpan-cpan-author:       ZMUGHAL
    gitpan-cpan-maturity:     released

---
+++ lib/Alien/Uninum.pm
@@ -0,0 +1,39 @@
+package Alien::Uninum;
+{
+  $Alien::Uninum::VERSION = '0.001';
+}
+# ABSTRACT: Alien package for the libuninum library
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Alien::Uninum - Alien package for the libuninum library
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Zakariyya Mughal <zmughal@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Zakariyya Mughal.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
